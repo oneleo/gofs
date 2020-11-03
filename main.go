10b4de1c67e8d9c5a03bcb2a5ce71822b3da54ad
oneleo <oneleo760823@gmail.com>
2020-11-04 06:55:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 // https://blog.wu-boy.com/2017/02/write-command-line-in-golang/
 // https://medium.com/@zamhuang/golang-%E5%A6%82%E4%BD%95%E8%AE%80%E5%8F%96-command-line-argument-flag-%E5%BF%85%E7%9F%A5%E7%9A%84%E5%B9%BE%E7%A8%AE%E7%94%A8%E6%B3%95-7dee79763a9e
+// https://darjun.github.io/2020/01/10/godailylib/flag/
 var (
 	port int64
 	path string
