314e35a3843a1f6b687eef213a5590840362fedd
oneleo <oneleo760823@gmail.com>
2020-11-04 20:15:01 +0800

    the first version of small file server

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # gofs
- 
+ Gofs is a small file server for Browsing specify local folder and files.
+
+## Install
+```golang
+$> go install github.com/oneleo/gofs
+```
+
+## Usage
+- Start this file server listen on port ":80" for browse "./" current folder.
+```bash
+$> gofs
+```
+
+- Start this file server listen on port ":80" for browse "./" current folder, and open in browser.
+```bash
+$> gofs -b
+```
+
+- Start this file server listen on port ":8081" for browse "./www" specify folder, and open in browser.
+```bash
+$> gofs -p 8081 -f ./www -b
+```
+
+- Help message
+```bash
+$> gofs -h
+OR
+$> gofs --help
+```
+
+```text
+$> gofs -h
+Gofs is a small file server for Browsing specify local folder and files.
+Usage:
+
+        gofs [-p port] [-f folder] [-b ture]
+
+Flags:
+  -b    Open URL in browser (shorthand)
+  -f string
+        Set shared folder (shorthand) (default "./")
+  -p int
+        Set listening port (shorthand) (default 80)
+
+Examples:
+  gofs
+        Start this file server listen on port ":80" for browse "./" current folder.
+  gofs -b
+        Start this file server listen on port ":80" for browse "./" current folder, and open in browser.
+  gofs -p 8081 -f ./www -b
+        Start this file server listen on port ":8081" for browse "./www" specify folder, and open in browser.
+```
+
+## References
+- [GitHub - Simple static file server](https://github.com/golang-id/gost)
+- [GitHub Gist - Open browser in golang](https://gist.github.com/hyg/9c4afcd91fe24316cbf0)
+- [Stackoverflow - Start web server to open page in browser](https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang)
--- main.go
+++ main.go
@@ -3,41 +3,100 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
-// https://blog.wu-boy.com/2017/02/write-command-line-in-golang/
-// https://medium.com/@zamhuang/golang-%E5%A6%82%E4%BD%95%E8%AE%80%E5%8F%96-command-line-argument-flag-%E5%BF%85%E7%9F%A5%E7%9A%84%E5%B9%BE%E7%A8%AE%E7%94%A8%E6%B3%95-7dee79763a9e
-// https://darjun.github.io/2020/01/10/godailylib/flag/
 var (
-	port   int64
-	folder string
+	port   *int64  = new(int64)
+	folder *string = new(string)
+	browse *bool   = new(bool)
 )
 
 func init() {
 	const (
-		defaultPort   = 80
+		defaultPort = 80
+		usagePort   = "Set listening port"
+
 		defaultFolder = "./"
-		usagePort     = "Set listening port"
 		usageFolder   = "Set shared folder"
+
+		defaultBrowse = false
+		usageBrowse   = "Open URL in browser"
 	)
-	//flag.Int64Var(&port, "port", defaultPort, usagePort)
-	flag.Int64Var(&port, "p", defaultPort, usagePort+" (shorthand)")
-	//flag.StringVar(&folder, "folder", defaultFolder, usageFolder)
-	flag.StringVar(&folder, "f", defaultFolder, usageFolder+" (shorthand)")
+	// flag.Int64Var(&port, "port", defaultPort, usagePort)
+	flag.Int64Var(port, "p", defaultPort, usagePort+" (shorthand)")
+	// flag.StringVar(&folder, "folder", defaultFolder, usageFolder)
+	flag.StringVar(folder, "f", defaultFolder, usageFolder+" (shorthand)")
+	// flag.BoolVar(browse, "browse", defaultBrowse, usageBrowse)
+	flag.BoolVar(browse, "b", defaultBrowse, usageBrowse+" (shorthand)")
 
 	flag.Usage = func() {
-		// example: go help doc
-		fmt.Fprintf(os.Stderr, "Gofs is a small file server for Browsing local folder and files.\n\n")
-		fmt.Fprintf(os.Stderr, "Usage:\n\n\tgofs [-p port] [-f folder]\n\n")
+		// template: go help doc
+		fmt.Fprintf(os.Stderr, "Gofs is a small file server for Browsing specify local folder and files.\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n\n\tgofs [-p port] [-f folder] [-b ture]\n\n")
 		fmt.Fprintf(os.Stderr, "Flags:\n")
 		flag.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "\nExamples:\n")
-		fmt.Fprintf(os.Stderr, "  gofs\n\tStart this file server and listen on port 80 for browse \"./\" folder.\n")
-		fmt.Fprintf(os.Stderr, "  gofs -p 8081 -r ./www\n\tStart this file server and listen on port 8081 for browse \"./www\" folder.")
+		fmt.Fprintf(os.Stderr, "  gofs\n\tStart this file server listen on port \":80\" for browse \"./\" current folder.\n")
+		fmt.Fprintf(os.Stderr, "  gofs -b\n\tStart this file server listen on port \":80\" for browse \"./\" current folder, and open in browser.\n")
+		fmt.Fprintf(os.Stderr, "  gofs -p 8081 -f ./www -b\n\tStart this file server listen on port \":8081\" for browse \"./www\" specify folder, and open in browser.")
+	}
+}
+
+// openURI will open URI in browser.
+func openURI(uri string) {
+	var c *string = new(string)
+	var args *[]string = new([]string)
+	var err *error = new(error)
+
+	// Obtain OS type.
+	switch runtime.GOOS {
+	case "windows":
+		// *c = "cmd"
+		// *args = []string{"/c", "start"}
+		*c = "rundll32"
+		*args = []string{"url.dll,FileProtocolHandler"}
+	case "darwin":
+		*c = "open"
+	case "linux":
+		*c = "xdg-open"
+	default:
+		log.Fatal("Unsupported operating system\n")
+	}
+
+	*args = append(*args, uri)
+	if *err != nil {
+		log.Fatal(*err, "\n")
+	}
+
+	*err = exec.Command(*c, *args...).Start()
+	if *err != nil {
+		log.Fatal(*err, "\n")
 	}
 }
 
 func main() {
 	flag.Parse()
+	// Obtain absolute folder even in different OS.
+	f, err := filepath.Abs(*folder)
+	if err != nil {
+		log.Fatal(err, "\n")
+	}
+
+	h := http.FileServer(http.Dir(f))
+	p := fmt.Sprintf(":%d", *port)
+	fmt.Print("File Server is Started to listen on localhost", p, " for browse ", f, "\nPlease press CTRL + C to close...\n")
+	// If '-b' flag is set, open URI in browser.
+	if *browse == true {
+		go openURI("http://127.0.0.1" + p)
+	}
+	// Start this file server
+	if err = http.ListenAndServe(p, h); err != nil {
+		log.Fatal(err, "\n")
+	}
 }
